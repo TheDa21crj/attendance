0574a4a4f04c43bca3c85113370aabeda1b95151
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-06 14:37:13 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,12 +3,13 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
+    console.log(process.env.mongoURL);
     await mongoose.connect(process.env.mongoURL, {
       useNewUrlParser: true,
     });
     console.log("DataBase Connected");
   } catch (error) {
-    console.log("error");
+    console.log(error);
   }
 };
 
