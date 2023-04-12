0c3fa406a5f3e032bbed184993fb036c85b4452c
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-12 21:08:20 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -5,17 +5,17 @@ const { check, validationResult } = require("express-validator");
 // Public
 router.post(
   "/start",
-  [check("payload", "payload is Required").not().isEmpty()],
+  [check("values", "values is Required").not().isEmpty()],
   async (req, res) => {
-    // const errors = validationResult(req);
-    // if (!errors.isEmpty()) {
-    //   return res.status(400).json({ errors: errors.array() });
-    // }
-    const { payload } = req.body;
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { values } = req.body;
 
     try {
-      console.log(payload);
-      return res.status(202).json({ message: payload });
+      console.log(values);
+      return res.status(202).json({ message: values });
     } catch (error) {
       console.log(error);
       return res.status(500).json({ message: error });
