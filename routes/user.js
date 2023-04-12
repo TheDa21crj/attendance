8fe88ec294d29689add2cabd84a03c6f11264915
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-12 21:01:16 +0530

    Update user.js

--- routes/user.js
+++ routes/user.js
@@ -7,10 +7,10 @@ router.post(
   "/start",
   [check("payload", "payload is Required").not().isEmpty()],
   async (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
+    // const errors = validationResult(req);
+    // if (!errors.isEmpty()) {
+    //   return res.status(400).json({ errors: errors.array() });
+    // }
     const { payload } = req.body;
 
     try {
