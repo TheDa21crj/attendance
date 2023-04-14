a3796aa1f85103498ef7f6017e6bcbd76b3c1945
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-14 13:28:13 +0530

    Update user.js

--- Schema/user.js
+++ Schema/user.js
@@ -15,6 +15,10 @@ const UserSchema = new mongoose.Schema({
     type: "string",
     required: true,
   },
+  start: {
+    type: "string",
+    default: "start",
+  },
   tokens: [
     {
       token: {
