2f0f58f08a97e0f37d71d3719da5248d79742b8e
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-14 13:14:51 +0530

    Create save.js

---
+++ routes/save.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const router = express.Router();
+const { check, validationResult } = require("express-validator");
+const gravatar = require("gravatar");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const User = require("./../Schema/user");
+
+// Public
+router.post(
+  "/start",
+  [check("values", "values is Required").not().isEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { values } = req.body;
+
+    try {
+      console.log(values);
+      return res.status(202).json("Success");
+    } catch (error) {
+      console.log(error);
+      return res.status(500).json({ message: error });
+    }
+  }
+);
+
+module.exports = router;
