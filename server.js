8f12cc2eef86af58f5a82c505308c3131056bb6b
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-14 14:10:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,11 +8,11 @@ const path = require("path");
 const app = express();
 
 app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
-  res.header(
-    "Access-Control-Allow-Origin",
-    "attendance-production-1373.up.railway.app"
-  );
+  res.header("Access-Control-Allow-Origin", process.env.AllowOrigin);
+  // res.header(
+  //   "Access-Control-Allow-Origin",
+  //   "attendance-production-1373.up.railway.app"
+  // );
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
