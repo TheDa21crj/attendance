616af614a2e4944ef3b59de2705f117225ce0446
Rishav Singh <rishavrajcrj@gmail.com>
2023-04-21 12:54:01 +0530

    cors update new

--- server.js
+++ server.js
@@ -7,13 +7,28 @@ const path = require("path");
 
 const app = express();
 
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", process.env.AllowOrigin);
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header(
+// app.use(function (req, res, next) {
+//   res.header("Access-Control-Allow-Origin", process.env.AllowOrigin);
+//   res.header("Access-Control-Allow-Credentials", true);
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PATCH, DELETE,OPTIONS"
+  );
+
   next();
 });
 
